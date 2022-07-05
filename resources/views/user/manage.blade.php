38777fc7c411eabbaf4ebbe1bd74b9a3eb682fb1
Irvanns <irvan_gm@yahoo.com>
2022-07-05 23:03:53 +0700

    Pembuatan popup tambah data

--- resources/views/user/manage.blade.php
+++ resources/views/user/manage.blade.php
@@ -62,5 +62,124 @@
     </div>
 </div>
 
+<div class="tambah_data_layer">
+    <div class="tambah_data_box">
+        <h2>
+            Penambahan Data Rapat
+        </h2>
+        <hr>
+        <div class="input_box">
+            <form action="">
+                <label for="judul_rapat">Judul Rapat</label>
+                <input type="text" id="judul_rapat" name="judul_rapat">
+            </form>
+        </div>
+        <div class="input_box">
+            <form action="">
+                <label for="tindak_lanjut">Tindak Lanjut Hasil Rapat</label>
+                <input type="text" id="tindak_lanjut" name="tindak_lanjut">
+            </form>
+        </div>
+        <div class="input_box">
+            <form action="">
+                <label for="penanggung_jawab">SKDP Penanggung Jawab</label>
+                <input type="text" id="penanggung_jawab" name="penanggung_jawab">
+            </form>
+        </div>
+        <div class="input_box">
+            <form action="">
+                <label for="progres_rapat">Progres</label>
+                <input type="text" id="progres_rapat" name="progres_rapat">
+            </form>
+        </div>
+        <div class="input_box">
+            <form action="">
+                <label for="data_pendukung">Data Pendukung</label>
+                <input type="text" id="data_pendukung" name="data_pendukung">
+            </form>
+        </div>
+        <div class="time">
+            <form action="">
+                <label for="batas_waktu">Batas Waktu</label>
+                <input type="date" id="batas_waktu" name="batas_waktu">
+            </form>
+        </div>
+        <div class="time">
+            <form action="">
+                <label for="waktu_rapat">Waktu Rapat</label>
+                <input type="date" id="waktu_rapat" name="waktu_rapat">
+            </form>
+        </div>
+        <div class="tombol_tambah">
+            <input type="submit" value="Tambah Data">
+        </div>
+    </div>
+</div>
+
+<div class="perubahan_data_layer">
+    <div class="tambah_data_box">
+        <h2>
+            Penambahan Data Rapat
+        </h2>
+        <hr>
+        <div class="input_box">
+            <form action="">
+                <label for="judul_rapat">Judul Rapat</label>
+                <input type="text" id="judul_rapat" name="judul_rapat">
+            </form>
+        </div>
+        <div class="input_box">
+            <form action="">
+                <label for="tindak_lanjut">Tindak Lanjut Hasil Rapat</label>
+                <input type="text" id="tindak_lanjut" name="tindak_lanjut">
+            </form>
+        </div>
+        <div class="input_box">
+            <form action="">
+                <label for="penanggung_jawab">SKDP Penanggung Jawab</label>
+                <input type="text" id="penanggung_jawab" name="penanggung_jawab">
+            </form>
+        </div>
+        <div class="input_box">
+            <form action="">
+                <label for="progres_rapat">Progres</label>
+                <input type="text" id="progres_rapat" name="progres_rapat">
+            </form>
+        </div>
+        <div class="input_box">
+            <form action="">
+                <label for="data_pendukung">Data Pendukung</label>
+                <input type="text" id="data_pendukung" name="data_pendukung">
+            </form>
+        </div>
+        <div class="time">
+            <form action="">
+                <label for="batas_waktu">Batas Waktu</label>
+                <input type="date" id="batas_waktu" name="batas_waktu">
+            </form>
+        </div>
+        <div class="time">
+            <form action="">
+                <label for="waktu_rapat">Waktu Rapat</label>
+                <input type="date" id="waktu_rapat" name="waktu_rapat">
+            </form>
+        </div>
+        <div class="perubahan_status_box">
+            <form action="">
+                <label for="perubahan_status">Status</label>
+                <select id="status" name="status">
+                    <option value="Selesai">Selesai</option>
+                    <option value="Belum Selesai">Belum Selesai</option>
+                </select>
+            </form>
+        </div>
+        <div class="tombol_selesai">
+            <input type="submit" value="Selesai">
+        </div>
+    </div>
+</div>
+
+
+
 {{-- ////////// --}}
 @endsection
