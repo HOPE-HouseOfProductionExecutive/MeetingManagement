72af90fc1e940422516261355bafa4467e1ebfa6
Irvanns <irvan_gm@yahoo.com>
2022-07-17 19:15:08 +0700

    pagination dashboard 70%

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <link rel="stylesheet" href="/Assets/css/user/dashboard/style.css">
 {{-- ISI DISINI --}}
-@foreach ($data as $item)
+<!-- @foreach ($data as $item)
 @php
     $time = \Carbon\Carbon::parse($item->waktu_rapat)->locale('id');
     $time->settings(['formatFunction' => 'translatedFormat']);
@@ -138,7 +138,164 @@
             @endforeach
         </div>
     </div>
-</div>
+</div> -->
+
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <script src="/Assets/js/pagination.js"></script>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="/Assets/css/user/pagination/style.css">
+
+<body onload="getData(0);">
+    
+    <section id="table">
+        <table>
+            <thead>
+                <th>No</th>
+                <th>Waktu</th>
+                <th>Judul</th>
+            </thead>
+            <tbody id="content-table-body">
+                
+            </tbody>
+        </table>
+    </section>
+    <footer class="center-align">
+        <ul class="pagination">
+            <li class="disabled arrow-left"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
+            <li class="active"><a href="#!">1</a></li>
+            <li class="waves-effect"><a href="#!">2</a></li>
+            <li class="waves-effect"><a href="#!">3</a></li>
+            <li class="waves-effect"><a href="#!">4</a></li>
+            <li class="waves-effect"><a href="#!">5</a></li>
+            <li class="waves-effect"><a href="#!">6</a></li>
+            <li class="waves-effect"><a href="#!">7</a></li>
+            <li class="waves-effect"><a href="#!">8</a></li>
+            <li class="waves-effect"><a href="#!">9</a></li>
+            <li class="waves-effect"><a href="#!">10</a></li>
+            <li class="waves-effect arrow-right"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
+        </ul>
+    </footer>
+
+<script>
+
+    function getData(index){
+        $.get('/pagination/ajax', function (data) {
+            // console.log(data);
+    
+            let tableBody = document.getElementById("content-table-body");
+            let total = 1;
+            let html= "";
+            for(let i = index; i < data.length && total <= 10 ; i++){
+                html += "<tr>";
+                
+                html += "<td>" + (i+1) + "</td>";
+                html += "<td>" + data[i].waktu_rapat + "</td>";
+                html += "<td>" + data[i].judul + "</td>";
+    
+                total += 1;
+                html += "</tr>";
+            }
+    
+            tableBody.innerHTML = html;
+    
+    
+        });
+    }
+
+    
+
+
+    function handleNumberClick (clickedLink, leftArrow, rightArrow)
+    {
+        
+        console.log(clickedLink);
+        clickedLink.parentElement.classList = "active";
+        let clickedLinkPageNumber = parseInt(clickedLink.innerText); 
+        console.log((clickedLinkPageNumber*10) - 10);
+        getData(((clickedLinkPageNumber * 10) - 10));
+        
+
+        // switch (clickedLinkPageNumber) {
+        //     case 1:
+        //         disableLeftArrow();
+        //         if(rightArrow.className.indexOf("disabled") !== -1) {
+        //             enableRightArrow();
+        //         }
+        //         break;
+        //     case 10:
+        //         break;
+        //     default:
+        //         break;
+        // }
+    }
+
+    function handleLeftArrowClick()
+    {
+
+    }
+
+    function handleRightArrowClick()
+    {
+
+    }
+
+    function disableLeftArrow(){
+        
+    }
+    function enableLeftArrow(){
+
+    }
+
+    function disableRightArrow(){
+        
+    }
+    function enableRightArrow(){
+
+    }
+
+    let pageLinks = document.querySelectorAll('a');
+    let activePageNumber;
+    let clickedLink;
+    let nextPage;
+    let leftArrow;
+    let rightArrow;
+    let url = '';
+    pageLinks.forEach((element) => {
+        element.addEventListener("click", function() {
+            leftArrow = document.querySelector('.left-arrow');
+            rightArrow = document.querySelector('.right-arrow');
+            activeLink = document.querySelector('.active');
+            
+            activePageNumber = parseInt(activeLink.innerText);
+            
+            if ((this.innerText === 'chevron_left' && activePageNumber === 1) || (this.innerText === 'chevron_right' && activePageNumber === 10)) {
+            return;
+            }
+
+            activeLink.classList = "waves-effect";
+            activeLink.classList.remove('active');
+
+            if(this.innerText === 'chevron_left'){
+                handleLeftArrowClick();
+            }else if (this.innerText === 'chevron_right'){
+                handleRightArrowClick();
+            }else {
+                handleNumberClick(this, leftArrow, rightArrow);
+            }
+
+        });
+        
+    });
+    
+</script>
+</html>
+
+
 
 <script>
     function onClickModalOpen(id){
