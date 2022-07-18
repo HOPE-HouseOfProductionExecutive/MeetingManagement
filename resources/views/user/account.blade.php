49dbdbb9178310394daf9f41d9885fec72bdc7fd
Irvanns <irvan_gm@yahoo.com>
2022-07-18 21:57:20 +0700

    account layout completed

--- resources/views/user/account.blade.php
+++ resources/views/user/account.blade.php
@@ -5,37 +5,44 @@
 <link rel="stylesheet" href="/Assets/css/user/account/style.css">
 
 <div class="akun_layer">
+    <div class="akun_upper">
+        <h3>Akun</h3>
+    </div>
     <div class="akun_box">
-        <div class="akun_upper">
-            <h3>Akun</h3>
+        <div class="box_upper">
+             <p>Rincian</p>
+             <p class="pr">Pemulihan Kata Sandi</p>
         </div>
-        <div class="akun_detail">
-            <div class="akun_kiri">
-                <div class="nama">
-                    <p>Nama Lengkap</p>
-                    <p>Muhammad Maulana Ibrahimovic</p>
-                </div>
-                <div class="bod">
-                    <p>Tanggal Lahir</p>
-                    <p>3 Maret 1997</p>
-                </div>
+        <div class="separate">
+            <div class="rincian">
+             <div class="rincian_nama">
+                 <p>Nama</p>
+                 <div class="nama_box">
+                     <h3 class="nb">Irvan Nur Hidayat</h3>
+                 </div>
+             </div>
+             <div class="rincian_nama">
+                 <p>Email</p>
+                 <div class="nama_box">
+                     <h3 class="nb">irvan@gmail.com</h3>
+                 </div>
+             </div>
+             <div class="rincian_nama">
+                 <p>Jabatan</p>
+                 <div class="nama_box">
+                     <h3 class="nb">Admin</h3>
+                 </div>
+             </div>
             </div>
-            <div class="akun_kanan">
-                <div class="email">
-                    <p>Email</p>
-                    <p>maulana.pegawaiumum@yahoo.com</p>
+            <div class="pemulihan">
+             <form action="">
+                 <input type="text" placeholder="Kata sandi saat ini">
+                 <input type="text" placeholder="Kata sandi baru">
+                 <input type="text" placeholder="Konfirmasi Kata sandi baru">
+                <div class="konfirm">
+                    <button>Konfirmasi</button>
                 </div>
-                <div class="password">
-                    <p>Ubah Kata Sandi</p>
-                    <form action="">
-                        <input type="text" placeholder="Kata sandi saat ini">
-                        <input type="text" placeholder="Kata sandi baru">
-                        <input type="text" placeholder="Konfirmasi Kata sandi baru">
-                            <div class="konfirmasi">
-                                <input type="submit" value="Konfirmasi">
-                            </div>
-                    </form>
-                </div>
+             </form>
             </div>
         </div>
     </div>
